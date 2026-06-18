d57d998f079b7be58c088e584f1226372fcb9b4b
olegkomel1-del <olegkomel1@gmail.com>
2026-06-18 16:22:18 +1000

    Create count-vm.tf

---
+++ Terraform/THW_3/Code task 1-5/count-vm.tf
@@ -0,0 +1,33 @@
+resource "yandex_compute_instance" "web_vm" {
+
+  count = 2
+
+  name        = "web-${count.index + 1}"
+  platform_id = var.vms_resources.web.platform_id
+
+  resources {
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size         = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+
+  security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = merge(var.vms_metadata, {
+    ssh-keys = local.ssh_key
+  })
+
+  depends_on = [yandex_compute_instance.db_vm]
+}
