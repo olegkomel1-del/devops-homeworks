6d670ded42be6425fa97cdc6051035cd71b81194
olegkomel1-del <olegkomel1@gmail.com>
2026-06-20 20:09:53 +1000

    Create disk_vm.tf

---
+++ Terraform/THW_3/Code task 6-9/disk_vm.tf
@@ -0,0 +1,45 @@
+resource "yandex_compute_disk" "storage_disks" {
+  count = 3
+  name = "storage-disk-${count.index + 1}"
+  zone = var.default_zone
+  size = 1
+}
+
+
+resource "yandex_compute_instance" "storage_vm" {
+  name        = "storage"
+  platform_id = var.vms_resources.web.platform_id
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = 10
+    }
+  }
+
+
+  dynamic "secondary_disk" {
+
+    for_each = yandex_compute_disk.storage_disks
+
+    content {
+
+      disk_id = secondary_disk.value.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = false
+  }
+
+  metadata = merge(var.vms_metadata, {
+    ssh-keys = local.ssh_key
+  })
+}
