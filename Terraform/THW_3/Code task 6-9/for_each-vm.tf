656a1efb5ac30c1e4cfc181b63e36fe89496fbda
olegkomel1-del <olegkomel1@gmail.com>
2026-06-20 20:10:46 +1000

    Create for_each-vm.tf

---
+++ Terraform/THW_3/Code task 6-9/for_each-vm.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "db_vm" {
+    for_each = { for vm in var.each_vm : vm.vm_name => vm }
+
+    name        = each.key
+  platform_id = var.vms_resources.web.platform_id
+
+  resources {
+    cores         = each.value.cpu
+    memory        = each.value.ram
+    core_fraction = each.value.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size         = each.value.disk_volume
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = false
+  }
+
+  metadata = merge(var.vms_metadata, {
+    ssh-keys = local.ssh_key
+  })
+}
